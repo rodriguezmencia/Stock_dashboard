2a05536df8f7c65608855162c21cac09ad583a18
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 09:30:37 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -19,6 +19,14 @@ st.markdown('''
 
 #Select what company do you want to analyze
 ''')
+
+with st.sidebar:
+    st.title("Enter information")
+    stock = st.text_input('Stock symbol', 'e.g.AAPL')
+    interval=st.selectbox('Enter the interval of time:',
+    ('1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'))
+    submit_code = st.form_button("Execute")
+
 st.title("Enter information")
 
 stock = st.text_input('Stock symbol', 'e.g.AAPL')
