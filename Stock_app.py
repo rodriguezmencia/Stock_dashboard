8532ec4acb6d56d75542f6bd85eb9034efc20036
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 16:09:51 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -92,7 +92,6 @@ if submit_code:
         stock_now=new_stock.iloc[-1]["Close"] #stock.history(period='1d').iloc[-1]["Close"]
         stock_beg=new_stock.iloc[-1]["Open"] #stock.history(period='1d').iloc[-1]["Open"]
         vol_now=new_stock.iloc[-1]["Volume"] #stock.history(period='1d').iloc[-1]["Volume"]
-        #date=stock.history(period='1d').reset_index().iloc[-1]["Date"]
         var=stock_now/stock_beg-1
         formatted_var = "{:.2%}".format(var)
         text_color = "green" if var > 0 else "red"
@@ -101,16 +100,16 @@ if submit_code:
         col1,col2,col3,col4 = st.columns([0.25,0.25,0.25,0.25])
         with col1:
             #st.plotly_chart(fig,use_container_width=True)
-            st.markdown("Price (USD)")
-            st.write(stock_now)
+            st.markdown(f"**Price (USD)**")
+            st.write(f'<p style="color:{black}">{stock_now}</p>', unsafe_allow_html=True)#st.write(stock_now)
         with col2:
             st.markdown(f"**Intraday var.%**")
             st.write(f'<p style="color:{text_color}">{formatted_var}</p>', unsafe_allow_html=True)
         with col3:
-            st.markdown("**Volume")
+            st.markdown(f"**Volume")
             st.write(vol_now)
         with col4:
-            st.markdown("**Date")
+            st.markdown(f"**Date")
             st.write(time)
         
         st.plotly_chart(fig,use_container_width=True)
