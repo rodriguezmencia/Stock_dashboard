c4db455a84bd9c7063fd09860041258b6b311917
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 18:18:20 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -147,32 +147,25 @@ if submit_code:
 	        data = c.fetchall()
 	        return data 
         data_struc = ['Date','Open','High','Low','Close','Volume','Dividends','Stock Splits']
-	
-	# Columns/Layout
 	col1,col2 = st.columns(2)
-
+	# query
 	with col1:
 		with st.form(key='query_form'):
 			raw_code = st.text_area("SQL Code Here")
 			submit_code = st.form_submit_button("Execute")
-
 		# Table of Info
-
 		with st.expander("Table Info"):
 			table_info = {'Stock infor':data_struc}
 			st.json(table_info)
-		
 	# Results Layouts
 	with col2:
 		if submit_code:
 			st.info("Query Submitted")
 			st.code(raw_code)
-
 			# Results 
 			query_results = sql_executor(raw_code)
 			with st.expander("Results"):
 				st.write(query_results)
-
 			with st.expander("Pretty Table"):
 				query_df = pd.DataFrame(query_results)
 				st.dataframe(query_df)
