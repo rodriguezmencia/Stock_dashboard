092ac1afcd5a390a61c42b950d9e51aa50cc8670
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 10:21:06 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -20,15 +20,14 @@ st.markdown('''
 #Select what company do you want to analyze
 ''')
 
-with st.sidebar:
+with st.sidebar.form(key ='Form1'):
     st.title("Enter information")
-    with st.form(key='test_email'):
-        symbol = st.text_input('Stock symbol', 'e.g.AAPL')
-        interval=st.selectbox('Enter the interval of time:',
-        ('1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'))
-        init = st.date_input("Enter the start date [YYYY-MM-DD]:", ) #datetime.date(2019, 7, 6)
-        finish=st.date_input("Enter the finish date [YYYY-MM-DD]:", )#datetime.date(2019, 7, 6)
-        submit_code = st.form_submit_button("Execute")
+    symbol = st.text_input('Stock symbol', 'e.g.AAPL')
+    interval=st.selectbox('Enter the interval of time:',
+    ('1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'))
+    init = st.date_input("Enter the start date [YYYY-MM-DD]:", ) #datetime.date(2019, 7, 6)
+    finish=st.date_input("Enter the finish date [YYYY-MM-DD]:", )#datetime.date(2019, 7, 6)
+    submit_code = st.form_submit_button("Execute")
 
 #if submit_code:
 #    st.write("Hello")
