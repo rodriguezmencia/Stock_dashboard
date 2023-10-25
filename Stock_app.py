38a4aec7d5e782436cfc7c31ef1dfc5128ffe473
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 11:06:47 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -124,97 +124,85 @@ if submit_code:
         
         
         # create a sql connection <---sólo 1 vez
-        con = sqlite3.connect('stock.db')
-        c = con.cursor()
+        #con = sqlite3.connect('stock.db')
+        #c = con.cursor()
         # create price table
-        query1 = """CREATE TABLE IF NOT EXISTS prices (
-        Date TEXT NOT NULL,
-        ticker TEXT NOT NULL,
-        price REAL,
-        PRIMARY KEY(Date, ticker)
-        )"""
-        c.execute(query1.replace('\n',' '))
+        #query1 = """CREATE TABLE IF NOT EXISTS prices (
+        #Date TEXT NOT NULL,
+        #ticker TEXT NOT NULL,
+        #price REAL,
+        #PRIMARY KEY(Date, ticker)
+        #)"""
+        #c.execute(query1.replace('\n',' '))
         # create volume table
-        query2 = """CREATE TABLE IF NOT EXISTS volume (
-        Date TEXT NOT NULL,
-        ticker TEXT NOT NULL,
-        volume REAL,
-        PRIMARY KEY(Date, ticker)
-        )"""
-        c.execute(query2.replace('\n',' '))
-        
-        
-        # In[ ]:
-        
+        #query2 = """CREATE TABLE IF NOT EXISTS volume (
+        #Date TEXT NOT NULL,
+        #ticker TEXT NOT NULL,
+        #volume REAL,
+        #PRIMARY KEY(Date, ticker)
+        #)"""
+        #c.execute(query2.replace('\n',' '))
         
         #adjuntar información en sqlite
-        adj_close_long.to_sql('prices', con, if_exists='append', index=False)
-        volume_long.to_sql('volume', con, if_exists='append', index=False)
-        
-        
-        # In[ ]:
-        
+        #adj_close_long.to_sql('prices', con, if_exists='append', index=False)
+        #volume_long.to_sql('volume', con, if_exists='append', index=False)
         
         #borrar información de sqlite después del uso
-        
-        
-        # In[ ]:
-        
         
         #hacer una consulta en sqlite
         
         # backtest inputs
-        bt_inputs = {'tickers': ['BA', 'UNH', 'MCD', 'HD'],
-        'start_date': '2019-01-01',
-        'end_date': '2021-06-01'}
+        #bt_inputs = {'tickers': ['BA', 'UNH', 'MCD', 'HD'],
+        #'start_date': '2019-01-01',
+        #'end_date': '2021-06-01'}
             
         # create a sql connection
-        con = sqlite3.connect('stock.db')
-        c = con.cursor()
+        #con = sqlite3.connect('stock.db')
+        #c = con.cursor()
         # create price table
-        query1 = """CREATE TABLE IF NOT EXISTS prices (
-        Date TEXT NOT NULL,
-        ticker TEXT NOT NULL,
-        price REAL,
-        PRIMARY KEY(Date, ticker)
-        )"""
-        c.execute(query1.replace('\n',' '))
+        #query1 = """CREATE TABLE IF NOT EXISTS prices (
+        #Date TEXT NOT NULL,
+        #ticker TEXT NOT NULL,
+        #price REAL,
+        #PRIMARY KEY(Date, ticker)
+        #)"""
+        #c.execute(query1.replace('\n',' '))
         # create volume table
-        query2 = """CREATE TABLE IF NOT EXISTS volume (
-        Date TEXT NOT NULL,
-        ticker TEXT NOT NULL,
-        volume REAL,
-        PRIMARY KEY(Date, ticker)
-        )"""
-        c.execute(query2.replace('\n',' '))
+        #query2 = """CREATE TABLE IF NOT EXISTS volume (
+        #Date TEXT NOT NULL,
+        #ticker TEXT NOT NULL,
+        #volume REAL,
+        #PRIMARY KEY(Date, ticker)
+        #)"""
+        #c.execute(query2.replace('\n',' '))
     
-        def download(bt_inputs, proxy = None):
-            data = yf.download(tickers= bt_inputs['tickers'],
-                               start = bt_inputs['start_date'],   
-                               end = bt_inputs['end_date'],
-                               interval = '1d',
-                               prepost = True,
-                               threads = True,
-                               proxy = proxy)
-            return data
+        #def download(bt_inputs, proxy = None):
+        #    data = yf.download(tickers= bt_inputs['tickers'],
+        #                       start = bt_inputs['start_date'],   
+        #                       end = bt_inputs['end_date'],
+        #                       interval = '1d',
+        #                       prepost = True,
+        #                       threads = True,
+        #                       proxy = proxy)
+        #    return data
         
-        test = download(bt_inputs)
+        #test = download(bt_inputs)
         
         
-        adj_close = test['Adj Close']
-        volume = test['Volume']
+        #adj_close = test['Adj Close']
+        #volume = test['Volume']
         
         
         # convert wide to long
-        adj_close_long = pd.melt(adj_close.reset_index(), id_vars='Date',
-                                 value_vars=bt_inputs['tickers'], var_name ="ticker", value_name="price")
-        volume_long = pd.melt(volume.reset_index(), id_vars='Date', value_vars=bt_inputs['tickers'],
-                              var_name = "ticker", value_name = "volume")
+        #adj_close_long = pd.melt(adj_close.reset_index(), id_vars='Date',
+        #                         value_vars=bt_inputs['tickers'], var_name ="ticker", value_name="price")
+        #volume_long = pd.melt(volume.reset_index(), id_vars='Date', value_vars=bt_inputs['tickers'],
+        #                      var_name = "ticker", value_name = "volume")
         
         
         
-        adj_close_long.to_sql('prices', con, if_exists='append', index=False)
-        volume_long.to_sql('volume', con, if_exists='append', index=False)
+        #adj_close_long.to_sql('prices', con, if_exists='append', index=False)
+        #volume_long.to_sql('volume', con, if_exists='append', index=False)
 
     else:
         st.write("please enter a valid stock symbol")
