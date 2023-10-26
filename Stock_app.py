eaab2c2cfe4f8b46f8905e2ddc9f4927ec814b18
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 22:37:55 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -145,15 +145,16 @@ with tab1:
             conn = sqlite3.connect('stock.sqlite')
             c = conn.cursor()
     with tab2:            
-    # Fxn Make Execution
+        # Fxn Make Execution
         def sql_executor(raw_code):
             c.execute(raw_code)
             data = c.fetchall()
             return data 
+            
         data_struc = ['Date','Open','High','Low','Close','Volume','Dividends','Stock Splits','MA_30','MA_15','MA_5']
 
         col5,col6 = st.columns(2)
-    # query
+        # query
         with col5:
             with st.form(key='query_form'):
                 raw_code = st.text_area("SQL Code Here")
@@ -164,16 +165,16 @@ with tab1:
                 st.json(table_info)
         # Results Layouts
         with col6:
-            #if submit_code2:
-            st.info("Query Submitted")
-            st.code(raw_code)
-            # Results 
-            query_results = sql_executor(raw_code)
-            with st.expander("Results"):
-                st.write(query_results)
-            with st.expander("Pretty Table"):
-                query_df = pd.DataFrame(query_results)
-                st.dataframe(query_df)
+            if submit_code2:
+                st.info("Query Submitted")
+                st.code(raw_code)
+                # Results 
+                query_results = sql_executor(raw_code)
+                with st.expander("Results"):
+                    st.write(query_results)
+                with st.expander("Pretty Table"):
+                    query_df = pd.DataFrame(query_results)
+                    st.dataframe(query_df)
         #----------------------------------------------------------------------------------------------
             
 
