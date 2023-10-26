acbe0e33c5bfd69f26998a4a8301668c4a58e976
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-25 23:48:03 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -14,6 +14,9 @@ from datetime import datetime
 import time
 import yfinance as yf
 
+st.set_page_config(
+    page_title="Stock app",
+)
 
 #Streamlit app
 #------------------------------------------------------------------------------------------
