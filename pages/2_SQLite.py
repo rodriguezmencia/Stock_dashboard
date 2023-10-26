a02465cc7146c81d798c8199b283f012cdbcb776
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-26 00:00:27 -0300

    Update 2_SQLite.py

--- pages/2_SQLite.py
+++ pages/2_SQLite.py
@@ -1,1 +1,17 @@
+#------------------------------
+#------import libraries--------
+#------------------------------
+import streamlit as st
+import pandas as pd
+import sqlite3
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import datetime
+from datetime import datetime
+import time
+import yfinance as yf
+
 st.set_page_config(page_title="SQLite Analysis", page_icon="📈")
