2287865497d963bfb043c042825457e512b2e5d1
Felipe Saavedra <felipe.saavedra@student.ie.edu>
2023-10-27 02:10:56 -0300

    Update Stock_app.py

--- Stock_app.py
+++ Stock_app.py
@@ -23,8 +23,8 @@ import yfinance as yf
 with st.sidebar.form(key ='Form1'):
     st.title("Enter information")
     symbol = st.text_input('Stock symbol e.g. GOOG',help='write down the stock symbol that you want to search',value='GOOG')
-    inter=st.selectbox('Enter the interval of time:',
-    ('1d', '5d', '1wk', '1mo', '3mo'))#'1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', 
+    #inter=st.selectbox('Enter the interval of time:',
+    #('1d', '5d', '1wk', '1mo', '3mo'))#'1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', 
     init = st.date_input("Enter the start date [YYYY/MM/DD]:",) 
     finish=st.date_input("Enter the finish date [YYYY/MM/DD]:", )
     
@@ -61,7 +61,7 @@ with tab1:
     # plot
     if submit_code:
         if symbol:
-            df=lee(symbol,inter,init,finish)
+            df=lee(symbol,'1d',init,finish)
     
             # Main plot
             fig = make_subplots(rows=2, cols=1,
